2c9e083b2b48a52e7e7ce5a2b65e1a949fd86fa8
Adrian Parker <ihsw.aparker@gmail.com>
2019-06-08 16:19:34 -0400

    Remove various files not used by dependencies.

--- packages/api/fn/test/src/index.ts
+++
@@ -1,6 +0,0 @@
-import {IPubsubMessage} from "@sotah/api/src/fn";
-
-export const test = (data: IPubsubMessage, _context: unknown) => {
-    // tslint:disable-next-line:no-console
-    console.log("Hello, world!", data);
-};
