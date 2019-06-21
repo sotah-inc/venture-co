b9fa9e1263ac2a0a5ccc1a416a853ec8be41f10a
Adrian Parker <ihsw.aparker@gmail.com>
2019-06-21 07:54:08 -0400

    Test out Hello component from client package.

--- packages/client-local-dev/src/App.tsx
+++ packages/client-local-dev/src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Hello } from "@sotah-inc/client";
+import React from "react";
+import "./App.css";
+import logo from "./logo.svg";
 
 const App: React.FC = () => {
   return (
@@ -19,8 +20,9 @@ const App: React.FC = () => {
           Learn React
         </a>
       </header>
+      <Hello />
     </div>
   );
-}
+};
 
 export default App;
