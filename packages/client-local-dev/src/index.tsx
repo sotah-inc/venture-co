834d0dedbd69461108db21eb2631424e63ea06df
Adrian Parker <ihsw.aparker@gmail.com>
2019-07-01 15:36:08 -0700

    Switch to default id naming.

--- packages/client-local-dev/src/index.tsx
+++ packages/client-local-dev/src/index.tsx
@@ -6,7 +6,7 @@ import "@sotah-inc/client/build/styles/venture-co.min.css";
 
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Boot />, document.getElementById("client-local-dev-container"));
+ReactDOM.render(<Boot />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
