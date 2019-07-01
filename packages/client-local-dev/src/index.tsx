ff657dbebe2d3294e13d3bc173d74fa7628f0c5b
Adrian Parker <ihsw.aparker@gmail.com>
2019-06-30 17:03:36 -0700

    Remove references to importing css.

--- packages/client-local-dev/src/index.tsx
+++ packages/client-local-dev/src/index.tsx
@@ -1,16 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "@blueprintjs/core/lib/css/blueprint.css";
-import "@blueprintjs/icons/lib/css/blueprint-icons.css";
-import "@blueprintjs/select/lib/css/blueprint-select.css";
 import { Boot } from "@sotah-inc/client";
-import "@sotah-inc/client/styles/App.scss";
-import "@sotah-inc/client/styles/AuctionTable.scss";
-import "@sotah-inc/client/styles/CreateEntryForm.scss";
-import "@sotah-inc/client/styles/ItemPopover.scss";
-import "@sotah-inc/client/styles/News.scss";
-import "@sotah-inc/client/styles/PriceLists.scss";
 
 import * as serviceWorker from "./serviceWorker";
 
