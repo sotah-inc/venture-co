63fb7c2e55069fa5d1923a08a82fb73bb7af971c
Adrian Parker <ihsw.aparker@gmail.com>
2019-08-25 16:32:05 -0700

    Fix member ordering.

--- packages/client/src/route-containers/App/Data/AuctionList/AuctionTable.tsx
+++ packages/client/src/route-containers/App/Data/AuctionList/AuctionTable.tsx
@@ -1,5 +1,28 @@
-import { withRouter } from "react-router-dom";
+import React from "react";
+
+import { RouteComponentProps, withRouter } from "react-router-dom";
 
 import { AuctionTableContainer } from "../../../../containers/App/Data/AuctionList/AuctionTable";
 
-export const AuctionTableRouteContainer = withRouter(AuctionTableContainer);
+type Props = Readonly<RouteComponentProps<{}>>;
+
+function RouteContainer({ history }: Props) {
+  return (
+    <AuctionTableContainer
+      browseToProfessionPricelist={(region, realm, profession, expansion, pricelist) => {
+        const professionPricelistUrl = [
+          "data",
+          region.name,
+          realm.slug,
+          "professions",
+          profession.name,
+          expansion.name,
+          pricelist.slug,
+        ].join("/");
+        history.push(`/${professionPricelistUrl}`);
+      }}
+    />
+  );
+}
+
+export const AuctionTableRouteContainer = withRouter(RouteContainer);
