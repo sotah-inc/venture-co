63846b3be3f0352c6d29569b0d6c8ba4c1194337
Adrian Parker <ihsw.aparker@gmail.com>
2019-08-27 19:25:30 -0700

    Refactor link-button component to use next-router rather than react-router.

--- packages/client/src/route-containers/util/LinkButton.tsx
+++ packages/client/src/route-containers/util/LinkButton.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 
-import { RouteComponentProps, withRouter } from "react-router-dom";
+import { WithRouterProps } from "next/dist/client/with-router";
+import { withRouter } from "next/router";
 
 import { IOwnProps, LinkButton } from "../../components/util/LinkButton";
 
-type Props = Readonly<RouteComponentProps<{}> & IOwnProps>;
+type Props = Readonly<IOwnProps & WithRouterProps>;
 
 function RouteContainer(props: Props) {
   // props
-  const { buttonProps, destination, history, location } = props;
+  const { buttonProps, destination, router } = props;
 
   return (
     <LinkButton
-      locationPathname={location.pathname}
-      historyPush={(pushDestination: string) => history.push(pushDestination)}
+      locationPathname={router.pathname}
+      historyPush={(pushDestination: string) => router.push(pushDestination)}
       destination={destination}
       buttonProps={buttonProps}
     />
