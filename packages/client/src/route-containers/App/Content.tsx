aecef5871f812557d8503759d461e7fe8339f8cb
Adrian Parker <ihsw.aparker@gmail.com>
2019-08-27 17:11:34 -0700

    Fix typo.

--- packages/client/src/route-containers/App/Content.tsx
+++ packages/client/src/route-containers/App/Content.tsx
@@ -7,7 +7,7 @@ import { Content } from "../../components/App/Content";
 type Props = Readonly<RouteComponentProps<{}>>;
 
 function RouteContainer({ history }: Props) {
-  return <Content browseToNews={() => history.replace("/content-news")} />;
+  return <Content browseToNews={() => history.replace("/content/news")} />;
 }
 
 export const ContentRouteContainer = withRouter(RouteContainer);
