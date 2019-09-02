1cd56671756a8b886f592f8fc916253ffc54da5c
Adrian Parker <ihsw.aparker@gmail.com>
2019-09-02 14:04:19 -0700

    Remove references to test code, start incorporating current route structure.

--- packages/next-local-dev/src/components/Layout.tsx
+++ packages/next-local-dev/src/components/Layout.tsx
@@ -1,77 +1,22 @@
 import React, { ReactNode } from "react";
 
+import { Boot } from "@sotah-inc/client";
 import Head from "next/head";
-
-import Nav from "../components/nav";
 
 interface IProps {
   children: ReactNode;
+  title: string;
 }
 
-export function Layout({ children }: Readonly<IProps>) {
+export function Layout({ children, title }: Readonly<IProps>) {
   return (
-    <div>
+    <>
       <Head>
-        <title>Home</title>
+        <title>{title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
       </Head>
-
-      <Nav />
-
-      <div className="hero">
-        <h1 className="title">Welcome to Next.js!</h1>
-        <p className="description">
-          To get started, edit <code>pages/index.js</code> and save to reload.
-        </p>
-
-        {children}
-      </div>
-
-      <style global={true} jsx={true}>{`
-        .hero {
-          width: 100%;
-          color: #333;
-        }
-        .title {
-          margin: 0;
-          width: 100%;
-          padding-top: 80px;
-          line-height: 1.15;
-          font-size: 48px;
-        }
-        .title,
-        .description {
-          text-align: center;
-        }
-        .row {
-          max-width: 880px;
-          margin: 80px auto 40px;
-          display: flex;
-          flex-direction: row;
-          justify-content: space-around;
-        }
-        .card {
-          padding: 18px 18px 24px;
-          width: 220px;
-          text-align: left;
-          text-decoration: none;
-          color: #434343;
-          border: 1px solid #9b9b9b;
-        }
-        .card:hover {
-          border-color: #067df7;
-        }
-        .card h3 {
-          margin: 0;
-          color: #067df7;
-          font-size: 18px;
-        }
-        .card p {
-          margin: 0;
-          padding: 12px 0 0;
-          font-size: 13px;
-          color: #333;
-        }
-      `}</style>
-    </div>
+      <Boot Viewport={children} />
+    </>
   );
 }
--- packages/next-local-dev/src/pages/index.tsx
+++ packages/next-local-dev/src/pages/index.tsx
@@ -1,40 +1,11 @@
 import React from "react";
 
-import Link from "next/link";
-
 import { Layout } from "../components/Layout";
 
 export function Home() {
   return (
-    <Layout>
-      <div className="row">
-        <a className="card" href="https://github.com/zeit/next.js#setup">
-          <h3>Getting Started &rarr;</h3>
-          <p>Learn more about Next.js on GitHub and in their examples.</p>
-        </a>
-        <a className="card" href="https://github.com/zeit/next.js/tree/master/examples">
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
-        <a className="card" href="https://github.com/zeit/next.js">
-          <h3>Create Next App &rarr;</h3>
-          <p>Was this tool helpful? Let us know how we can improve it!</p>
-        </a>
-      </div>
-      <div className="row">
-        <Link href="/post/[pid]" as="/post/wew-lad">
-          <a className="card">
-            <h3>Wew lad</h3>
-            <p>El oh el</p>
-          </a>
-        </Link>
-        <Link href="/sotah" as="/sotah">
-          <a className="card">
-            <h3>SotAH</h3>
-            <p>SotAH</p>
-          </a>
-        </Link>
-      </div>
+    <Layout title="Secrets of the Auction House">
+      <p>Hello, world!</p>
     </Layout>
   );
 }
