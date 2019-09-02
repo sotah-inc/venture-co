0476814703be0765e5caf3be6d65b5811890d457
Adrian Parker <ihsw.aparker@gmail.com>
2019-09-02 15:34:12 -0700

    Update to latest dep, start overriding predefined state.

--- packages/next-local-dev/src/pages/content/news.tsx
+++ packages/next-local-dev/src/pages/content/news.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 
+import { defaultState } from "@sotah-inc/client";
 import { NewsRouteContainer } from "@sotah-inc/client/build/dist/route-containers/App/Content/News";
+import { defaultMainState } from "@sotah-inc/client/build/dist/types";
+import { FetchLevel } from "@sotah-inc/client/build/dist/types/main";
 
 import { Layout } from "../../components/Layout";
 
 export function Content() {
   return (
-    <Layout title="Secrets of the Auction House">
+    <Layout
+      title="Secrets of the Auction House"
+      predefinedState={{
+        ...defaultState,
+        Main: { ...defaultMainState, fetchPingLevel: FetchLevel.success },
+      }}
+    >
       <NewsRouteContainer />
     </Layout>
   );
