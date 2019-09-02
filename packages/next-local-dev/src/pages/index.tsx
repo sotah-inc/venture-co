576dcff021c38aa1760de6bd8254859cc99bd0a2
Adrian Parker <ihsw.aparker@gmail.com>
2019-09-02 15:13:43 -0700

    Start incorporating override of default state.

--- packages/next-local-dev/src/pages/index.tsx
+++ packages/next-local-dev/src/pages/index.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 
-import { RootRouteContainer } from "@sotah-inc/client/build/dist/route-containers/App/Root";
+import { defaultState } from "@sotah-inc/client";
+import { defaultMainState } from "@sotah-inc/client/build/dist/types";
+import { FetchLevel } from "@sotah-inc/client/build/dist/types/main";
 
 import { Layout } from "../components/Layout";
 
 export function Home() {
   return (
-    <Layout title="Secrets of the Auction House">
-      <RootRouteContainer />
+    <Layout
+      title="Secrets of the Auction House"
+      predefinedState={{
+        ...defaultState,
+        Main: { ...defaultMainState, fetchPingLevel: FetchLevel.success },
+      }}
+    >
+      <p>Hello, world!</p>
     </Layout>
   );
 }
