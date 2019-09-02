4dccf9875a29a11e0a3cb1a04cc1d831738c38ea
Adrian Parker <ihsw.aparker@gmail.com>
2019-09-02 09:23:16 -0700

    Continue building out next-based file structure.

--- packages/next-local-dev/src/pages/sotah.tsx
+++ packages/next-local-dev/src/pages/sotah.tsx
@@ -1,9 +1,20 @@
 import * as React from "react";
 
 import { Boot } from "@sotah-inc/client";
+import "@sotah-inc/client/build/styles/venture-co.min.css";
+import Head from "next/head";
 
 export function Sotah() {
-  return <Boot />;
+  return (
+    <div>
+      <Boot />
+      <Head>
+        <title>SotAH</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+      </Head>
+    </div>
+  );
 }
 
 export default Sotah;
