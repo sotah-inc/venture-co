e9cc02095004e5210ae1bca0c756cc9c121b830c
Adrian Parker <ihsw.aparker@gmail.com>
2019-09-09 14:26:50 -0700

    Add missing func for initializing a db connection.

--- packages/server/src/db/index.ts
+++ packages/server/src/db/index.ts
@@ -1,3 +1,11 @@
+import { createConnection } from "typeorm";
+
+import { Post } from "../../../api/src/entities/post";
+import { Preference } from "../../../api/src/entities/preference";
+import { Pricelist } from "../../../api/src/entities/pricelist";
+import { PricelistEntry } from "../../../api/src/entities/pricelist-entry";
+import { ProfessionPricelist } from "../../../api/src/entities/profession-pricelist";
+import { User } from "../../../api/src/entities/user";
 export * from "./entities";
 
 export interface IDatabaseSettings {
@@ -8,3 +16,25 @@ export interface IDatabaseSettings {
   password: string;
   port: number;
 }
+
+export const connectDatabase = ({
+  dbName,
+  dbHostname,
+  connectionName,
+  password,
+  port,
+  username,
+}: IDatabaseSettings) => {
+  return createConnection({
+    database: dbName,
+    entities: [Preference, Pricelist, PricelistEntry, ProfessionPricelist, User, Post],
+    host: dbHostname,
+    logging: false,
+    name: connectionName,
+    password,
+    port,
+    synchronize: false,
+    type: "postgres",
+    username,
+  });
+};
