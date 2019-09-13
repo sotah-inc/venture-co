0eac2686bcfdc458ba889daa583b486c3f428b33
Adrian Parker <ihsw.aparker@gmail.com>
2019-09-13 09:50:19 -0700

    Start preference repository.

---
+++ packages/server/src/db/entities/preference-repository.ts
@@ -0,0 +1,19 @@
+import { AbstractRepository, EntityRepository } from "typeorm";
+
+import { Preference } from "./preference";
+
+@EntityRepository(Preference)
+export class PreferenceRepository extends AbstractRepository<Preference> {
+  public async getFromUserId(userId: number): Promise<Preference | null> {
+    const preference = await this.repository
+      .createQueryBuilder("preference")
+      .innerJoinAndSelect("preference.user", "user", "user.id = :user_id", { user_id: userId })
+      .getOne();
+
+    if (typeof preference === "undefined") {
+      return null;
+    }
+
+    return preference;
+  }
+}
