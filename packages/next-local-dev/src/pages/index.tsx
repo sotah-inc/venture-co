e6d70614c1f03d618fcbde06774e9712390bf8b1
Adrian Parker <ihsw.aparker@gmail.com>
2019-09-16 14:11:20 -0700

    Start adding predefined state.

--- packages/next-local-dev/src/pages/index.tsx
+++ packages/next-local-dev/src/pages/index.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 
+import { defaultState } from "@sotah-inc/client";
+import { defaultMainState } from "@sotah-inc/client/build/dist/types";
+import { FetchLevel } from "@sotah-inc/client/build/dist/types/main";
 import { Layout } from "../components/Layout";
 
 export function Home() {
   return (
-    <Layout title="Secrets of the Auction House">
+    <Layout
+      title="Secrets of the Auction House"
+      predefinedState={{
+        ...defaultState,
+        Main: { ...defaultMainState, fetchPingLevel: FetchLevel.success },
+      }}
+    >
       <p>Hello, world!</p>
     </Layout>
   );
