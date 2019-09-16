522618c2a73dd4656261a88a090820b5a5db8420
Adrian Parker <ihsw.aparker@gmail.com>
2019-09-16 15:44:31 -0700

    Continue integrating server-side functionality to next project.

--- packages/next-local-dev/src/pages/content/news.tsx
+++ packages/next-local-dev/src/pages/content/news.tsx
@@ -1,27 +1,41 @@
 import React from "react";
 
-import { NewsRouteContainer } from "@sotah-inc/client/build/dist/route-containers/App/Content/News";
-
 import { defaultState } from "@sotah-inc/client";
+import { ReceiveGetBoot } from "@sotah-inc/client/build/dist/actions/main";
+import { handlers } from "@sotah-inc/client/build/dist/reducers/handlers";
+import { NewsRouteContainer } from "@sotah-inc/client/build/dist/route-containers/App/Content/News";
 import { defaultMainState } from "@sotah-inc/client/build/dist/types";
-import { FetchLevel } from "@sotah-inc/client/build/dist/types/main";
-import { Layout } from "../../components/Layout";
+import { code } from "@sotah-inc/server";
+import { IGetBootResponse } from "@sotah-inc/server/build/dist/messenger/contracts";
 
-export function Content() {
+import { Layout } from "../../components/Layout";
+import { getMessengerClient } from "../../lib";
+
+interface IInitialProps {
+  boot: IGetBootResponse | null;
+}
+
+export function Content({ boot }: IInitialProps) {
   return (
     <Layout
       title="Secrets of the Auction House"
       predefinedState={{
         ...defaultState,
-        Main: {
-          ...defaultMainState,
-          fetchPingLevel: FetchLevel.success,
-        },
+        Main: handlers.main["boot"]["get"]["receive"](defaultMainState, ReceiveGetBoot(boot)),
       }}
     >
       <NewsRouteContainer />
     </Layout>
   );
 }
+
+Content.getInitialProps = async (): Promise<IInitialProps> => {
+  const msg = await (await getMessengerClient()).getBoot();
+  if (msg.code !== code.ok) {
+    return { boot: null };
+  }
+
+  return { boot: msg.data };
+};
 
 export default Content;
