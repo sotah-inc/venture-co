a95cc3507d2c3887c79a3da77416bab07dbbca3e
Adrian Parker <ihsw.aparker@gmail.com>
2019-09-16 19:56:46 -0700

    Create convenience func.

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,12 @@
+import process from "process";
+
 export * from "./types";
+
+export const getEnvVar = (envVarName: string): string => {
+  const envVar = process.env[envVarName];
+  if (typeof envVar === "undefined") {
+    return "";
+  }
+
+  return envVar;
+};
