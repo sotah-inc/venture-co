4647805d927d8cc8f63efe332ec65c800d24f48d
Adrian Parker <ihsw.aparker@gmail.com>
2019-09-17 20:03:18 -0700

    Add fetching of news posts.

--- packages/next-local-dev/src/pages/content/news.tsx
+++ packages/next-local-dev/src/pages/content/news.tsx
@@ -2,19 +2,21 @@ import React from "react";
 
 import { defaultState } from "@sotah-inc/client";
 import { ReceiveGetBoot, ReceiveGetPing } from "@sotah-inc/client/build/dist/actions/main";
-import { getBoot } from "@sotah-inc/client/build/dist/api/data";
+import { ReceiveGetPosts } from "@sotah-inc/client/build/dist/actions/posts";
+import { getBoot, getPosts, IGetPostsResult } from "@sotah-inc/client/build/dist/api/data";
 import { runners } from "@sotah-inc/client/build/dist/reducers/handlers";
 import { NewsRouteContainer } from "@sotah-inc/client/build/dist/route-containers/App/Content/News";
-import { defaultMainState } from "@sotah-inc/client/build/dist/types";
+import { defaultMainState, defaultPostsState } from "@sotah-inc/client/build/dist/types";
 import { IGetBootResponse } from "@sotah-inc/server/build/dist/messenger/contracts";
 
 import { Layout } from "../../components/Layout";
 
 interface IInitialProps {
   boot: IGetBootResponse | null;
+  posts: IGetPostsResult | null;
 }
 
-export function Content({ boot }: IInitialProps) {
+export function Content({ boot, posts }: IInitialProps) {
   return (
     <Layout
       title="Secrets of the Auction House"
@@ -24,6 +26,7 @@ export function Content({ boot }: IInitialProps) {
           runners.main(defaultMainState, ReceiveGetPing(true)),
           ReceiveGetBoot(boot),
         ),
+        Posts: runners.post(defaultPostsState, ReceiveGetPosts(posts)),
       }}
     >
       <NewsRouteContainer />
@@ -32,7 +35,7 @@ export function Content({ boot }: IInitialProps) {
 }
 
 Content.getInitialProps = async (): Promise<IInitialProps> => {
-  return { boot: await getBoot() };
+  return { boot: await getBoot(), posts: await getPosts() };
 };
 
 export default Content;
