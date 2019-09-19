0c6b5d4a534764379ad7cb67eda444748d7291f5
Adrian Parker <ihsw.aparker@gmail.com>
2019-09-18 18:56:07 -0700

    Create convenience component for oven.

---
+++ packages/client/src/components/util/Oven.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import { IToastProps, Position, Toaster } from "@blueprintjs/core";
+
+export interface IStateProps {
+  toast: IToastProps;
+  index: number;
+}
+
+type Props = Readonly<IStateProps>;
+
+export class Oven extends React.Component<Props> {
+  // @ts-ignore
+  private toaster: Toaster;
+  private refHandlers = {
+    toaster: (ref: Toaster) => {
+      this.toaster = ref;
+    },
+  };
+
+  public componentDidUpdate(prevProps: Props) {
+    // props
+    const { index, toast } = this.props;
+
+    if (index === prevProps.index) {
+      return;
+    }
+
+    this.toaster.show(toast);
+  }
+
+  public render() {
+    return <Toaster position={Position.TOP_RIGHT} ref={this.refHandlers.toaster} />;
+  }
+}
