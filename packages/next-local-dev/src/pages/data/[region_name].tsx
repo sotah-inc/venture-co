a0f911628e4de56a5f504a08d089240476980d13
Adrian Parker <ihsw.aparker@gmail.com>
2019-09-30 15:46:57 -0700

    Start data section.

---
+++ packages/next-local-dev/src/pages/data/[region_name].tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import { ReceiveGetBoot, ReceiveGetPing } from "@sotah-inc/client/build/dist/actions/main";
+import { getBoot } from "@sotah-inc/client/build/dist/api/data";
+import { runners } from "@sotah-inc/client/build/dist/reducers/handlers";
+// tslint:disable-next-line:max-line-length
+import { RegionRouteContainer } from "@sotah-inc/client/build/dist/route-containers/App/Data/Region";
+import { defaultMainState, IStoreState } from "@sotah-inc/client/build/dist/types";
+import { IGetBootResponse } from "@sotah-inc/core";
+import { NextPageContext } from "next";
+
+import { Layout } from "../../components/Layout";
+
+interface IInitialProps {
+  data?: {
+    boot: IGetBootResponse | null;
+  };
+}
+
+export function Region({ data }: Readonly<IInitialProps>) {
+  const predefinedState: Partial<IStoreState> | undefined = (() => {
+    if (typeof data === "undefined") {
+      return;
+    }
+
+    return {
+      Main: runners.main(
+        runners.main(defaultMainState, ReceiveGetPing(true)),
+        ReceiveGetBoot(data.boot),
+      ),
+    };
+  })();
+
+  return (
+    <Layout title="Secrets of the Auction House" predefinedState={predefinedState}>
+      <RegionRouteContainer />
+    </Layout>
+  );
+}
+
+Region.getInitialProps = async ({ req }: NextPageContext): Promise<IInitialProps> => {
+  if (typeof req === "undefined") {
+    return {};
+  }
+
+  return { data: { boot: await getBoot() } };
+};
+
+export default Region;
