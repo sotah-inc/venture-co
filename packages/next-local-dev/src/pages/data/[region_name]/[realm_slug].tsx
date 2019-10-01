6e4cff77a9505d07d8a5f863daf03b1ece51252c
Adrian Parker <ihsw.aparker@gmail.com>
2019-10-01 07:21:19 -0700

    Create realm-slug component.

---
+++ packages/next-local-dev/src/pages/data/[region_name]/[realm_slug].tsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+import {
+  ReceiveGetBoot,
+  ReceiveGetPing,
+  ReceiveGetRealms,
+} from "@sotah-inc/client/build/dist/actions/main";
+import { getBoot, getStatus } from "@sotah-inc/client/build/dist/api/data";
+import { runners } from "@sotah-inc/client/build/dist/reducers/handlers";
+import { RealmRouteContainer } from "@sotah-inc/client/build/dist/route-containers/App/Data/Realm";
+import { defaultMainState, IStoreState } from "@sotah-inc/client/build/dist/types";
+import { extractString } from "@sotah-inc/client/build/dist/util";
+import { IGetBootResponse, IStatusRealm } from "@sotah-inc/core";
+import { NextPageContext } from "next";
+
+import { Layout } from "../../../components/Layout";
+
+interface IInitialProps {
+  data?: {
+    boot: IGetBootResponse | null;
+    realms: IStatusRealm[] | null;
+  };
+}
+
+export function Realm({ data }: Readonly<IInitialProps>) {
+  const predefinedState: Partial<IStoreState> | undefined = (() => {
+    if (typeof data === "undefined") {
+      return;
+    }
+
+    return {
+      Main: runners.main(
+        runners.main(
+          runners.main(defaultMainState, ReceiveGetPing(true)),
+          ReceiveGetBoot(data.boot),
+        ),
+        ReceiveGetRealms(data.realms),
+      ),
+    };
+  })();
+
+  return (
+    <Layout title="Secrets of the Auction House" predefinedState={predefinedState}>
+      <RealmRouteContainer />
+    </Layout>
+  );
+}
+
+Realm.getInitialProps = async ({ req, query }: NextPageContext): Promise<IInitialProps> => {
+  if (typeof req === "undefined") {
+    return {};
+  }
+
+  const [boot, realms] = await Promise.all([
+    getBoot(),
+    getStatus(extractString("region_name", query)),
+  ]);
+
+  return {
+    data: {
+      boot,
+      realms,
+    },
+  };
+};
+
+export default Realm;
