f211783fb2417619f0e7ed3ccf3e482564d6ae84
Adrian Parker <ihsw.aparker@gmail.com>
2019-10-04 13:15:50 -0700

    Fix typing.

--- packages/client/src/route-containers/App/ProfessionsLanding.tsx
+++ packages/client/src/route-containers/App/ProfessionsLanding.tsx
@@ -3,10 +3,9 @@ import React from "react";
 import { WithRouterProps } from "next/dist/client/with-router";
 import { withRouter } from "next/router";
 
-import { IRouteProps } from "../../components/App/ProfessionsLanding";
 import { ProfessionsLandingContainer } from "../../containers/App/ProfessionsLanding";
 
-type Props = Readonly<IRouteProps & WithRouterProps>;
+type Props = Readonly<WithRouterProps>;
 
 function RouteContainer({ router }: Props) {
   return (
