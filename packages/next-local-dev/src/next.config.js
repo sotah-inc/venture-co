a78038c1d216bfe87bc0d72415d97b1e1e976dc2
Adrian Parker <ihsw.aparker@gmail.com>
2019-10-07 20:37:21 -0700

    Switch back to using env var in nextjs config (holy shit the next.config.js does not support async calls, crufty old bullshit.)

--- packages/next-local-dev/src/next.config.js
+++ packages/next-local-dev/src/next.config.js
@@ -1,5 +1,5 @@
 const withCSS = require('@zeit/next-css');
-const sotahServer = require("@sotah-inc/server");
+const sotahCore = require("@sotah-inc/core");
 
 module.exports = withCSS({
   cssModules: true,
@@ -7,9 +7,9 @@ module.exports = withCSS({
     localIdentName: "[local]",
   },
   publicRuntimeConfig: {
-    publicApiEndpoint: sotahServer.getConfig("public_api_endpoint", "PUBLIC_API_ENDPOINT")
+    publicApiEndpoint: sotahCore.getEnvVar("PUBLIC_API_ENDPOINT")
   },
   serverRuntimeConfig: {
-    serverApiEndpoint: sotahServer.getConfig("server_api_endpoint", "SERVER_API_ENDPOINT")
+    serverApiEndpoint: sotahCore.getEnvVar("SERVER_API_ENDPOINT")
   }
 });
