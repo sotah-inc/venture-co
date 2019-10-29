d8d81c5b5abc1aef1ecc1474a905d02c24f57eb4
Adrian Parker <ihsw.aparker@gmail.com>
2019-10-28 20:37:45 -0700

    Clean up root entry-point component.

--- packages/client/src/components/entry-point/Root.tsx
+++ packages/client/src/components/entry-point/Root.tsx
@@ -1,85 +1,21 @@
 import React from "react";
 
 import { Classes, Intent, NonIdealState, Spinner } from "@blueprintjs/core";
-import { ILoadRootEntrypoint } from "../../actions/main";
-import { FetchLevel } from "../../types/main";
 
 export interface IRouteProps {
   redirectToContent: () => void;
 }
 
-export interface IDispatchProps {
-  loadRootEntrypoint: (payload: ILoadRootEntrypoint) => void;
-}
-
-export interface IStateProps {
-  fetchPingLevel: FetchLevel;
-  fetchBootLevel: FetchLevel;
-}
-
-export interface IOwnProps {
-  rootEntrypointData?: ILoadRootEntrypoint;
-}
-
-export type Props = Readonly<IStateProps & IDispatchProps & IOwnProps & IRouteProps>;
+export type Props = Readonly<IRouteProps>;
 
 export class Root extends React.Component<Props> {
   public componentDidMount() {
-    const {
-      fetchBootLevel,
-      fetchPingLevel,
-      loadRootEntrypoint,
-      rootEntrypointData,
-      redirectToContent,
-    } = this.props;
+    const { redirectToContent } = this.props;
 
-    if (fetchPingLevel === FetchLevel.success && fetchBootLevel === FetchLevel.success) {
-      redirectToContent();
-
-      return;
-    }
-
-    const shouldLoad =
-      fetchPingLevel === FetchLevel.initial && fetchBootLevel === FetchLevel.initial;
-    if (shouldLoad && typeof rootEntrypointData !== "undefined") {
-      loadRootEntrypoint(rootEntrypointData);
-
-      return;
-    }
-  }
-
-  public componentDidUpdate(_prevProps: Props) {
-    const { fetchBootLevel, fetchPingLevel, redirectToContent } = this.props;
-
-    if (fetchPingLevel === FetchLevel.success && fetchBootLevel === FetchLevel.success) {
-      redirectToContent();
-
-      return;
-    }
+    redirectToContent();
   }
 
   public render() {
-    // props
-    const { fetchPingLevel, fetchBootLevel } = this.props;
-
-    if (fetchPingLevel !== FetchLevel.success) {
-      return (
-        <NonIdealState
-          title="Connecting..."
-          icon={<Spinner className={Classes.LARGE} intent={Intent.NONE} />}
-        />
-      );
-    }
-
-    if (fetchBootLevel !== FetchLevel.success) {
-      return (
-        <NonIdealState
-          title="Booting..."
-          icon={<Spinner className={Classes.LARGE} intent={Intent.NONE} />}
-        />
-      );
-    }
-
     return (
       <NonIdealState
         title="Redirecting to content!"
--- packages/client/src/route-containers/entry-point/Root.tsx
+++ packages/client/src/route-containers/entry-point/Root.tsx
@@ -3,18 +3,12 @@ import React from "react";
 import { WithRouterProps } from "next/dist/client/with-router";
 import { withRouter } from "next/router";
 
-import { IOwnProps } from "../../components/entry-point/Root";
-import { RootContainer } from "../../containers/entry-point/Root";
+import { Root } from "../../components/entry-point/Root";
 
-type Props = Readonly<WithRouterProps & IOwnProps>;
+type Props = Readonly<WithRouterProps>;
 
-function RouteContainer({ rootEntrypointData, router }: Props) {
-  return (
-    <RootContainer
-      rootEntrypointData={rootEntrypointData}
-      redirectToContent={() => router.replace("/content")}
-    />
-  );
+function RouteContainer({ router }: Props) {
+  return <Root redirectToContent={() => router.replace("/content")} />;
 }
 
 export const RootRouteContainer = withRouter(RouteContainer);
