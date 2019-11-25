5b72b18efab8def05c2f356abe039926c930c6e9
Adrian Parker <ihsw.aparker@gmail.com>
2019-11-24 19:04:40 -0700

    Fill in expected load-auctionlist-entrypoint handler func

--- packages/client/src/reducers/handlers/auction.ts
+++ packages/client/src/reducers/handlers/auction.ts
@@ -1,8 +1,45 @@
+import { IAuction } from "@sotah-inc/core";
+
 import { AuctionActions } from "../../actions";
+import { LoadAuctionListEntrypoint } from "../../actions/auction";
 import { IAuctionState } from "../../types/auction";
+import { FetchLevel } from "../../types/main";
 import { IKindHandlers, Runner } from "./index";
 
-export const handlers: IKindHandlers<IAuctionState, AuctionActions> = {};
+export const handlers: IKindHandlers<IAuctionState, AuctionActions> = {
+  entrypoint: {
+    auctionlist: {
+      load: (state: IAuctionState, action: ReturnType<typeof LoadAuctionListEntrypoint>) => {
+        if (action.payload.auctions === null) {
+          return { ...state, fetchAuctionsLevel: FetchLevel.failure };
+        }
+
+        const auctions: IAuction[] = (() => {
+          if (action.payload.auctions.auctions === null) {
+            return [];
+          }
+
+          return action.payload.auctions.auctions;
+        })();
+
+        if (action.payload.auctionsQuery === null) {
+          return { ...state, queryAuctionsLevel: FetchLevel.failure };
+        }
+
+        return {
+          ...state,
+          auctions,
+          fetchAuctionsLevel: FetchLevel.success,
+          items: action.payload.auctions.items,
+          queryAuctionResults: action.payload.auctionsQuery.items,
+          queryAuctionsLevel: FetchLevel.success,
+          relatedProfessionPricelists: action.payload.auctions.professionPricelists,
+          totalResults: action.payload.auctions.total,
+        };
+      },
+    },
+  },
+};
 
 export const run: Runner<IAuctionState, AuctionActions> = (
   state: IAuctionState,
