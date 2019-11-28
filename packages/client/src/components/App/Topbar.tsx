b14564008c061a94db0a4a80e638dc6d0db9d4f5
Adrian Parker <ihsw.aparker@gmail.com>
2019-11-28 12:50:30 -0700

    Add missing.

--- packages/client/src/components/App/Topbar.tsx
+++ packages/client/src/components/App/Topbar.tsx
@@ -209,7 +209,8 @@ export class Topbar extends React.Component<Props> {
 
     return (
       <LinkButtonRouteContainer
-        destination={`/data/${currentRegion.name}/${currentRealm.slug}${destination}`}
+        destination={`/data/[region_name]/[realm_slug]${destination}`}
+        asDestination={`/data/${currentRegion.name}/${currentRealm.slug}${destination}`}
         buttonProps={{ icon, text, minimal: true }}
       />
     );
