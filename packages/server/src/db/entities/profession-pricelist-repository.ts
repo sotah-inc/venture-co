35d85719f067269abcdd757c58b3ee96f49dc9d4
Adrian Parker <ihsw.aparker@gmail.com>
2019-12-10 06:42:32 -0700

    Add helpful func for fetching profession-pricelist individually.

--- packages/server/src/db/entities/profession-pricelist-repository.ts
+++ packages/server/src/db/entities/profession-pricelist-repository.ts
@@ -1,5 +1,6 @@
 import { AbstractRepository, EntityRepository } from "typeorm";
 
+import { ExpansionName, ProfessionName } from "@sotah-inc/core";
 import { ProfessionPricelist } from "./profession-pricelist";
 
 @EntityRepository(ProfessionPricelist)
@@ -12,6 +13,32 @@ export class ProfessionPricelistRepository extends AbstractRepository<Profession
         "pricelist",
         "pricelist.id = :pricelist_id",
         { pricelist_id: pricelistId },
+      )
+      .innerJoinAndSelect("pricelist.user", "pricelist.user")
+      .innerJoinAndSelect("pricelist.entries", "pricelist.entries");
+
+    const professionPricelist = await queryBuilder.getOne();
+
+    if (typeof professionPricelist === "undefined") {
+      return null;
+    }
+
+    return professionPricelist;
+  }
+
+  public async getFromPricelistSlug(
+    profession: ProfessionName,
+    expansion: ExpansionName,
+    slug: string,
+  ): Promise<ProfessionPricelist | null> {
+    const queryBuilder = this.repository
+      .createQueryBuilder("profession_pricelist")
+      .where({ name: profession, expansion })
+      .innerJoinAndSelect(
+        "profession_pricelist.pricelist",
+        "pricelist",
+        "pricelist.slug = :pricelist_slug",
+        { pricelist_slug: slug },
       )
       .innerJoinAndSelect("pricelist.user", "pricelist.user")
       .innerJoinAndSelect("pricelist.entries", "pricelist.entries");
