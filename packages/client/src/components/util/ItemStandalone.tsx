3a913e5afb00edd1834ddde0834b1c995dc68025
Adrian Parker <ihsw.aparker@gmail.com>
2020-01-12 15:49:07 -0700

    Start filling in expected item functionality.

--- packages/client/src/components/util/ItemStandalone.tsx
+++ packages/client/src/components/util/ItemStandalone.tsx
@@ -1,18 +1,59 @@
 import React from "react";
 
+import { Position } from "@blueprintjs/core";
 import { IItem, ItemId } from "@sotah-inc/core";
+
+import { ItemPopoverContainer } from "../../containers/util/ItemPopover";
 import { IFetchData } from "../../types/global";
+import { FetchLevel } from "../../types/main";
+import { qualityToColorClass } from "../../util";
 
 export interface IOwnProps {
   itemId: ItemId;
 }
 
 interface IState {
-  item: IFetchData<IItem>;
+  item: IFetchData<IItem | null>;
 }
 
 export class ItemStandalone extends React.Component<IOwnProps, IState> {
+  public state: IState = {
+    item: {
+      data: null,
+      errors: {},
+      level: FetchLevel.initial,
+    },
+  };
+
   public render() {
-    return <p>Hello, world!</p>;
+    const {
+      item: { level, errors, data: item },
+    } = this.state;
+
+    switch (level) {
+      case FetchLevel.success:
+        if (item === null) {
+          return <em>Item fetch succeeded but item was null.</em>;
+        }
+
+        return (
+          <ItemPopoverContainer
+            interactive={false}
+            onItemClick={() => {
+              return;
+            }}
+            position={Position.BOTTOM}
+            item={item}
+            itemTextFormatter={v => <span className={qualityToColorClass(item.quality)}>{v}</span>}
+          />
+        );
+      case FetchLevel.failure:
+        return <em>Failed to fetch item: {errors.error}</em>;
+      case FetchLevel.fetching:
+        return <em>Fetching item...</em>;
+      case FetchLevel.initial:
+      default:
+        return <em>Loading item...</em>;
+    }
   }
 }
