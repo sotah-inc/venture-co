ee3334437959637507377ad4a491d7206207aabd
Adrian Parker <ihsw.aparker@gmail.com>
2020-01-19 20:03:21 -0700

    Add rendering of x- and y-axis.

--- packages/client/src/components/entry-point/News/TokenHistoryGraph.tsx
+++ packages/client/src/components/entry-point/News/TokenHistoryGraph.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 
-import { CartesianGrid, LineChart, ResponsiveContainer, XAxis } from "recharts";
+import { CartesianGrid, LineChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
 
 import { IFetchData } from "../../../types/global";
 import { FetchLevel } from "../../../types/main";
 import { IRegionTokenHistories } from "../../../types/posts";
 import {
   convertRegionTokenHistoriesToLineData,
+  currencyToText,
   getXAxisTimeRestrictions,
   unixTimestampToText,
+  zeroGraphValue,
 } from "../../../util";
 
 export interface IStateProps {
@@ -18,6 +20,54 @@ export interface IStateProps {
 type Props = Readonly<IStateProps>;
 
 export class TokenHistoryGraph extends React.Component<Props> {
+  private static renderXAxis() {
+    const { xAxisTicks, roundedNowDate, roundedTwoWeeksAgoDate } = getXAxisTimeRestrictions();
+
+    return (
+      <XAxis
+        dataKey="name"
+        tickFormatter={unixTimestampToText}
+        domain={[roundedTwoWeeksAgoDate.unix(), roundedNowDate.unix()]}
+        type="number"
+        ticks={xAxisTicks}
+        tick={{ fill: "#fff" }}
+      />
+    );
+  }
+
+  private static renderYAxis() {
+    return (
+      <YAxis
+        tickFormatter={v => currencyToText(v * 10 * 10)}
+        domain={[
+          dataMin => {
+            if (dataMin <= 1) {
+              return zeroGraphValue;
+            }
+
+            const result = Math.pow(10, Math.floor(Math.log10(dataMin)));
+            if (result === 0) {
+              return zeroGraphValue;
+            }
+
+            return result;
+          },
+          dataMax => {
+            if (dataMax <= 1) {
+              return 10;
+            }
+
+            return Math.pow(10, Math.ceil(Math.log10(dataMax)));
+          },
+        ]}
+        tick={{ fill: "#fff" }}
+        scale="log"
+        allowDataOverflow={true}
+        mirror={true}
+      />
+    );
+  }
+
   public render() {
     const { regionTokenHistories } = this.props;
 
@@ -32,20 +82,13 @@ export class TokenHistoryGraph extends React.Component<Props> {
     }
 
     const data = convertRegionTokenHistoriesToLineData(regionTokenHistories.data);
-    const { xAxisTicks, roundedNowDate, roundedTwoWeeksAgoDate } = getXAxisTimeRestrictions();
 
     return (
       <ResponsiveContainer width="100%" height={250}>
         <LineChart data={data}>
           <CartesianGrid vertical={false} strokeWidth={0.25} strokeOpacity={0.25} />
-          <XAxis
-            dataKey="name"
-            tickFormatter={unixTimestampToText}
-            domain={[roundedTwoWeeksAgoDate.unix(), roundedNowDate.unix()]}
-            type="number"
-            ticks={xAxisTicks}
-            tick={{ fill: "#fff" }}
-          />
+          {TokenHistoryGraph.renderXAxis()}
+          {TokenHistoryGraph.renderYAxis()}
         </LineChart>
       </ResponsiveContainer>
     );
