403fd360edfbb7f7973e72cc7deb2f03c145e9fa
Adrian Parker <ihsw.aparker@gmail.com>
2020-01-30 07:27:03 -0700

    Start auction-stats-graph component.

---
+++ packages/client/src/components/entry-point/News/AuctionStatsGraph.tsx
@@ -0,0 +1,244 @@
+import React from "react";
+
+import { Icon, Intent, Tag } from "@blueprintjs/core";
+import { IconNames } from "@blueprintjs/icons";
+import { RegionName } from "@sotah-inc/core";
+import moment from "moment";
+import { CartesianGrid, Line, LineChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
+
+import { IFetchData, ILineItemOpen } from "../../../types/global";
+import { FetchLevel } from "../../../types/main";
+import { IRegionTokenHistories } from "../../../types/posts";
+import {
+  convertRegionTokenHistoriesToLineData,
+  currencyToText,
+  getColor,
+  unixTimestampToText,
+  zeroGraphValue,
+} from "../../../util";
+
+export interface IStateProps {
+  regionTokenHistories: IFetchData<IRegionTokenHistories>;
+}
+
+type Props = Readonly<IStateProps>;
+
+type State = Readonly<{
+  highlightedRegionName: RegionName | null;
+}>;
+
+const numberOfDays = 4;
+const nowDate = moment();
+const roundedEarliestDateLimit = moment()
+  .subtract(nowDate.hours(), "hours")
+  .subtract(nowDate.minutes(), "minutes")
+  .subtract(nowDate.seconds(), "seconds")
+  .subtract(numberOfDays, "days");
+const roundedNowDate = moment()
+  .subtract(nowDate.hours(), "hours")
+  .subtract(nowDate.minutes(), "minutes")
+  .subtract(nowDate.seconds(), "seconds")
+  .add(1, "days");
+
+export class AuctionStatsGraph extends React.Component<Props, State> {
+  private static renderXAxis() {
+    const xAxisTicks = Array.from(Array(numberOfDays + 2)).map(
+      (_, i) => roundedEarliestDateLimit.unix() + i * 60 * 60 * 24,
+    );
+
+    return (
+      <XAxis
+        dataKey="name"
+        tickFormatter={unixTimestampToText}
+        domain={[roundedEarliestDateLimit.unix(), roundedNowDate.unix()]}
+        type="number"
+        ticks={xAxisTicks}
+        tick={{ fill: "#fff" }}
+      />
+    );
+  }
+
+  private static renderYAxis() {
+    return (
+      <YAxis
+        tickFormatter={currencyToText}
+        domain={[
+          dataMin => {
+            if (dataMin <= 1) {
+              return zeroGraphValue;
+            }
+
+            const result = Math.pow(10, Math.floor(Math.log10(dataMin)));
+            if (result === 0) {
+              return zeroGraphValue;
+            }
+
+            return result;
+          },
+          dataMax => {
+            if (dataMax <= 1) {
+              return 10;
+            }
+
+            return Math.pow(10, Math.ceil(Math.log10(dataMax)));
+          },
+        ]}
+        tick={{ fill: "#fff" }}
+        scale="log"
+        allowDataOverflow={true}
+        mirror={true}
+      />
+    );
+  }
+
+  private static getDataKey(regionName: RegionName) {
+    return `${regionName}_token_price`;
+  }
+
+  public state: State = {
+    highlightedRegionName: null,
+  };
+
+  public componentDidMount() {
+    this.setState({
+      highlightedRegionName: null,
+    });
+  }
+
+  public render() {
+    const { regionTokenHistories } = this.props;
+
+    switch (regionTokenHistories.level) {
+      case FetchLevel.success:
+        break;
+      case FetchLevel.failure:
+        return <p>Failed to fetch token-histories!</p>;
+      case FetchLevel.fetching:
+      default:
+        return <p>Fetching regional token-histories...</p>;
+    }
+
+    const data = convertRegionTokenHistoriesToLineData(regionTokenHistories.data).filter(
+      v => v.name > roundedEarliestDateLimit.unix(),
+    );
+
+    return (
+      <>
+        <ResponsiveContainer width="100%" height={250}>
+          <LineChart data={data}>
+            <CartesianGrid vertical={false} strokeWidth={0.25} strokeOpacity={0.25} />
+            {AuctionStatsGraph.renderXAxis()}
+            {AuctionStatsGraph.renderYAxis()}
+            {this.renderLines()}
+          </LineChart>
+        </ResponsiveContainer>
+        {this.renderLegend()}
+      </>
+    );
+  }
+
+  private renderLegendColumn(regionIndexTuples: Array<[RegionName, number]>, index: number) {
+    return (
+      <div className="pure-u-1-2" key={index}>
+        <div style={index < 1 ? { marginRight: "10px" } : {}}>
+          {regionIndexTuples.map(([regionName, originalIndex], i) =>
+            this.renderLegendColumnTag(regionName, originalIndex, i),
+          )}
+        </div>
+      </div>
+    );
+  }
+
+  private renderLegendColumnTag(regionName: RegionName, originalIndex: number, i: number) {
+    const { intent, rightIcon, interactive } = (() => {
+      const rightIconElement = <Icon icon={IconNames.CHART} color={getColor(originalIndex)} />;
+
+      return { intent: Intent.NONE, rightIcon: rightIconElement, interactive: false };
+    })();
+
+    return (
+      <Tag
+        fill={true}
+        key={i}
+        minimal={true}
+        style={{ marginBottom: "5px" }}
+        intent={intent}
+        rightIcon={rightIcon}
+        interactive={interactive}
+        onMouseEnter={() => {
+          this.setState({ ...this.state, highlightedRegionName: regionName });
+        }}
+        onMouseLeave={() => {
+          this.setState({ ...this.state, highlightedRegionName: null });
+        }}
+      >
+        {this.renderLegendItem(regionName)}
+      </Tag>
+    );
+  }
+
+  private renderLegendItem(regionName: RegionName) {
+    return <span>{regionName}</span>;
+  }
+
+  private renderLine(index: number, regionName: RegionName) {
+    const { highlightedRegionName } = this.state;
+
+    const { stroke, strokeWidth } = (() => {
+      if (highlightedRegionName === null || highlightedRegionName === regionName) {
+        return {
+          stroke: getColor(index),
+          strokeWidth: highlightedRegionName === regionName ? 4 : 2,
+        };
+      }
+
+      return {
+        stroke: "#5C7080",
+        strokeWidth: 1,
+      };
+    })();
+
+    return (
+      <Line
+        key={index}
+        name={regionName}
+        dataKey={(item: ILineItemOpen) => item.data[AuctionStatsGraph.getDataKey(regionName)]}
+        animationDuration={500}
+        animationEasing={"ease-in-out"}
+        type={"basis"}
+        connectNulls={true}
+        stroke={stroke}
+        strokeWidth={strokeWidth}
+      />
+    );
+  }
+
+  private renderLines() {
+    const { regionTokenHistories } = this.props;
+
+    return Object.keys(regionTokenHistories.data).map((v, i) => this.renderLine(i, v));
+  }
+
+  private renderLegend() {
+    const { regionTokenHistories } = this.props;
+
+    const groupedRegionNames = Object.keys(regionTokenHistories.data).reduce<
+      Array<Array<[RegionName, number]>>
+    >((result, v, i) => {
+      const column = i % 2;
+      if (Object.keys(result).indexOf(column.toString()) === -1) {
+        result[column] = [];
+      }
+
+      result[column].push([v, i]);
+
+      return result;
+    }, []);
+
+    return (
+      <div className="pure-g">
+        {groupedRegionNames.map((v, i) => this.renderLegendColumn(v, i))}
+      </div>
+    );
+  }
+}
