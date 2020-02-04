856619008fe669b5284a3fe42c2397d4c2b0aa07
Adrian Parker <ihsw.aparker@gmail.com>
2020-02-04 07:40:19 -0700

    Fill in remaining work-order json definition.

--- packages/core/src/types/work-order.ts
+++ packages/core/src/types/work-order.ts
@@ -1,4 +1,14 @@
+import { ItemId } from "./item";
+import { RealmSlug, RegionName } from "./region";
+
 export interface IWorkOrderJson {
   id: number;
   user_id: number;
+  region_name: RegionName;
+  realm_slug: RealmSlug;
+  item_id: ItemId;
+  quantity: number;
+  price: number;
+  created_at: number;
+  updated_at: number;
 }
