8734c7c51b2e67cc4d53aaa5b307d654414be152
Adrian Parker <ihsw.aparker@gmail.com>
2020-02-07 07:51:47 -0700

    Continue filling in query-work-orders controller func.

--- packages/api/src/controllers/work-order.ts
+++ packages/api/src/controllers/work-order.ts
@@ -1,8 +1,13 @@
-import { IQueryWorkOrdersResponse, IValidationErrorResponse } from "@sotah-inc/core";
+import {
+  IQueryWorkOrdersParams,
+  IQueryWorkOrdersResponse,
+  IValidationErrorResponse,
+} from "@sotah-inc/core";
 import { Messenger } from "@sotah-inc/server";
 import HTTPStatus from "http-status";
 import { Connection } from "typeorm";
 
+import { QueryWorkOrdersParamsRules } from "../lib/validator-rules";
 import { QueryRequestHandler } from "./index";
 
 export class WorkOrderController {
@@ -17,6 +22,19 @@ export class WorkOrderController {
   public queryWorkOrders: QueryRequestHandler<
     IQueryWorkOrdersResponse | IValidationErrorResponse
   > = async req => {
+    // parsing request params
+    let result: IQueryWorkOrdersParams | null = null;
+    try {
+      result = await QueryWorkOrdersParamsRules.validate(req.query);
+    } catch (err) {
+      const validationErrors: IValidationErrorResponse = { [err.path]: err.message };
+
+      return {
+        data: validationErrors,
+        status: HTTPStatus.BAD_REQUEST,
+      };
+    }
+
     return {
       status: HTTPStatus.OK,
     };
