4f4dec62ad45fdaf4f5fe716a64e2e6ffcc8c616
Adrian Parker <ihsw.aparker@gmail.com>
2020-02-09 08:45:49 -0700

    Add missing work-order entity.

--- packages/server/src/db/index.ts
+++ packages/server/src/db/index.ts
@@ -1,6 +1,14 @@
 import { Connection, createConnection } from "typeorm";
 
-import { Post, Preference, Pricelist, PricelistEntry, ProfessionPricelist, User } from "./entities";
+import {
+  Post,
+  Preference,
+  Pricelist,
+  PricelistEntry,
+  ProfessionPricelist,
+  User,
+  WorkOrder,
+} from "./entities";
 
 export * from "./entities";
 
@@ -15,7 +23,7 @@ export interface IDatabaseSettings {
 
 type ConnectDatabaseFn = (opts: IDatabaseSettings) => Promise<Connection>;
 
-export const connectDatabase: ConnectDatabaseFn = ({
+export const connectDatabase: ConnectDatabaseFn = async ({
   dbName,
   dbHostname,
   connectionName,
@@ -25,7 +33,7 @@ export const connectDatabase: ConnectDatabaseFn = ({
 }) => {
   return createConnection({
     database: dbName,
-    entities: [Preference, Pricelist, PricelistEntry, ProfessionPricelist, User, Post],
+    entities: [Preference, Pricelist, PricelistEntry, ProfessionPricelist, User, Post, WorkOrder],
     host: dbHostname,
     logging: false,
     name: connectionName,
