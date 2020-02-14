ec99c5553e8d42f5eb87c6626e6441b7bfe5a5a0
Adrian Parker <ihsw.aparker@gmail.com>
2020-02-14 12:39:42 -0700

    Start filling in additional relevant work-order state.

--- packages/client/src/types/work-order.ts
+++ packages/client/src/types/work-order.ts
@@ -1,22 +1,26 @@
-import { ITokenHistory, IWorkOrderJson } from "@sotah-inc/core";
+import { IWorkOrderJson, OrderDirection, OrderKind, SortPerPage } from "@sotah-inc/core";
 
 import { IFetchData } from "./global";
 import { FetchLevel } from "./main";
 
-export interface IRegionTokenHistories {
-  [regionName: string]: ITokenHistory | undefined;
-}
-
 export interface IWorkOrderState {
   loadId: string;
   orders: IFetchData<IWorkOrderJson[]>;
+  perPage: SortPerPage;
+  currentPage: number;
+  orderBy: OrderKind;
+  orderDirection: OrderDirection;
 }
 
 export const defaultWorkOrderState: IWorkOrderState = {
+  currentPage: 1,
   loadId: "",
+  orderBy: OrderKind.CreatedAt,
+  orderDirection: OrderDirection.Desc,
   orders: {
     data: [],
     errors: {},
     level: FetchLevel.initial,
   },
+  perPage: SortPerPage.Fifty,
 };
