357160451449783dba4958af2192c0b0f91cf789
Adrian Parker <ihsw.aparker@gmail.com>
2020-09-16 17:01:39 -0700

    Add missing.

--- packages/client/src/reducers/price-lists.ts
+++ packages/client/src/reducers/price-lists.ts
@@ -1,4 +1,10 @@
 import { PriceListsActions } from "../actions";
+import {
+  CHANGE_IS_ADD_ENTRY_DIALOG_OPEN,
+  CHANGE_IS_ADD_LIST_DIALOG_OPEN,
+  CHANGE_IS_DELETE_LIST_DIALOG_OPEN,
+  CHANGE_IS_EDIT_LIST_DIALOG_OPEN,
+} from "../actions/price-lists";
 import { defaultPriceListsState, IPriceListsState } from "../types/price-lists";
 import { runners } from "./handlers";
 
@@ -10,6 +16,26 @@ export const priceLists = (state: State | undefined, action: PriceListsActions):
   }
 
   switch (action.type) {
+    case CHANGE_IS_ADD_LIST_DIALOG_OPEN:
+      return {
+        ...state,
+        isAddListDialogOpen: action.payload,
+      };
+    case CHANGE_IS_EDIT_LIST_DIALOG_OPEN:
+      return {
+        ...state,
+        isEditListDialogOpen: action.payload,
+      };
+    case CHANGE_IS_DELETE_LIST_DIALOG_OPEN:
+      return {
+        ...state,
+        isDeleteListDialogOpen: action.payload,
+      };
+    case CHANGE_IS_ADD_ENTRY_DIALOG_OPEN:
+      return {
+        ...state,
+        isAddListDialogOpen: action.payload,
+      };
     default:
       return runners.pricelist(state, action);
   }
