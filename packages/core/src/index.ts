e68b7ef730f6f01d2939024a7c4222e37a1e6dec
Adrian Parker <ihsw.aparker@gmail.com>
2020-09-18 07:56:26 -0700

    Remove usage of zero-graph-value.

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,4 @@
 import process from "process";
-import { zeroGraphValue } from "../../client/src/util";
 
 export * from "./types";
 
@@ -18,10 +17,8 @@ export interface ILimits {
 }
 
 export function normalizeLimits(input: ILimits): ILimits {
-  const nextOverallLower = Math.pow(10, Math.floor(Math.log10(input.lower)));
-
   return {
-    lower: nextOverallLower === 0 ? zeroGraphValue : nextOverallLower,
+    lower: Math.pow(10, Math.floor(Math.log10(input.lower))),
     upper: Math.pow(10, Math.ceil(Math.log10(input.upper))),
   };
 }
