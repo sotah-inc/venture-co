06a1b4a9d692b8e59cdd4b35f941acdbea908c64
Adrian Parker <ihsw.aparker@gmail.com>
2020-10-09 16:01:53 -0700

    Continue adding playable-class enums.

--- packages/core/src/types/short-item.ts
+++ packages/core/src/types/short-item.ts
@@ -19,10 +19,12 @@ export enum PlayableClass {
   Warrior = 1,
   Paladin = 2,
   Hunter = 3,
+  Rogue = 4,
   Priest = 5,
   DeathKnight = 6,
   Shaman = 7,
   Mage = 8,
+  Warlock = 9,
   Monk = 10,
   Druid = 11,
 }
