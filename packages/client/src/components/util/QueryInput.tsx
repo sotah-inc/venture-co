f308cd5f3c26687cdb90b50ed1c412a399e527d0
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2020-10-21 09:06:01 -0700

    Continue query-input component.

--- packages/client/src/components/util/QueryInput.tsx
+++ packages/client/src/components/util/QueryInput.tsx
@@ -1,121 +1,49 @@
 import React, { useState } from "react";
 
 import { Classes, H6, Menu, MenuItem } from "@blueprintjs/core";
-import {
-  IItemListRendererProps,
-  IItemRendererProps,
-  ItemListRenderer,
-  ItemPredicate,
-  Suggest,
-} from "@blueprintjs/select";
-import { IQueryItem, IQueryItemWithId, IShortItem, ItemId, Locale } from "@sotah-inc/core";
+import { IItemListRendererProps, IItemRendererProps, Suggest } from "@blueprintjs/select";
+import { IQueryItem, IQueryItemWithId, Locale } from "@sotah-inc/core";
 import { debounce } from "lodash";
 
 import { getItems } from "../../api/data";
-import { getItemIconUrl, getItemTextValue, qualityToColorClass } from "../../util";
+import { qualityToColorClass } from "../../util";
 
-const ItemSuggest<T> = Suggest.ofType<IQueryItem<T extends IQueryItemWithId>>();
-
-export interface IOwnProps<T> {
+interface IOwnProps<T extends IQueryItemWithId> {
   autoFocus?: boolean;
   idBlacklist?: number[];
   closeOnSelect?: boolean;
-  itemIdActiveList?: number[];
+  idActiveList?: number[];
   initialResults?: Array<IQueryItem<T>>;
 
   onSelect(item: T): void;
+  inputValueRenderer(result: IQueryItem<T>): string;
+  renderLabel(result: IQueryItem<T>): string | undefined;
+  renderText(result: IQueryItem<T>): React.ReactNode;
 }
 
-type Props<T> = Readonly<IOwnProps<T>>;
+type Props<T extends IQueryItemWithId> = Readonly<IOwnProps<T>>;
 
-function inputValueRenderer<T>(result: IQueryItem<T>): string {
-  if (result.item === null || result.item.id === 0) {
-    return "n/a";
-  }
-
-  return getItemTextValue(result.item);
-}
-
-function renderItemAsItemRendererText(item: IShortItem) {
-  const itemText = getItemTextValue(item);
-  const itemIconUrl = getItemIconUrl(item);
-
-  if (itemIconUrl === null) {
-    return itemText;
-  }
-
-  return (
-    <>
-      <img src={itemIconUrl} className="item-icon" alt="" /> {itemText}
-    </>
-  );
-}
-
-function renderItemRendererTextContent(item: IShortItem | null) {
-  if (item === null || item.id === 0) {
-    return "n/a";
-  }
-
-  return renderItemAsItemRendererText(item);
-}
-
-function renderItemRendererText(item: IShortItem | null) {
-  return <span className="item-input-menu-item">{renderItemRendererTextContent(item)}</span>;
-}
-
-const itemPredicate: ItemPredicate<IQueryItemsItem> = (_: string, result: IQueryItemsItem) => {
-  return result.rank > -1;
-};
-
-const itemListRenderer: ItemListRenderer<IQueryItemsItem> = (
-  params: IItemListRendererProps<IQueryItemsItem>,
-) => {
-  const { items, itemsParentRef, renderItem } = params;
-  const renderedItems = items.map(renderItem).filter(renderedItem => renderedItem !== null);
-  if (renderedItems.length === 0) {
-    return (
-      <Menu ulRef={itemsParentRef}>
-        <li>
-          <H6>Queried Results</H6>
-        </li>
-        <li>
-          <em>No results found.</em>
-        </li>
-      </Menu>
-    );
-  }
-
-  return (
-    <Menu ulRef={itemsParentRef} className="item-input-menu">
-      <li>
-        <H6>Queried Results</H6>
-      </li>
-      {renderedItems}
-    </Menu>
-  );
-};
-
-export function ItemInput(props: Props) {
+export function renderQuerySuggest<T extends IQueryItemWithId>(props: Props<T>) {
   const {
-    autoFocus,
+    inputValueRenderer,
+    idBlacklist,
+    renderLabel,
+    idActiveList,
+    renderText,
     onSelect,
-    closeOnSelect,
-    itemIdBlacklist,
-    itemIdActiveList,
-    initialResults,
   } = props;
 
-  const [results, setResults] = useState<IQueryItemsItem[]>(initialResults ?? []);
+  const SuggestElement = Suggest.ofType<IQueryItem<T>>();
 
   return (
-    <ItemSuggest
+    <SuggestElement
       inputValueRenderer={inputValueRenderer}
       itemRenderer={(result, itemRendererProps: IItemRendererProps) => {
         const { handleClick, modifiers, index } = itemRendererProps;
         const { item } = result;
 
         const disabled: boolean = (() => {
-          if (typeof itemIdBlacklist === "undefined") {
+          if (typeof idBlacklist === "undefined") {
             return false;
           }
 
@@ -123,19 +51,16 @@ export function ItemInput(props: Props) {
             return true;
           }
 
-          return itemIdBlacklist.includes(item.id);
+          return idBlacklist.includes(item.id);
         })();
 
         if (!modifiers.matchesPredicate) {
           return null;
         }
 
-        const hasFullItem = (item?.sotah_meta.normalized_name.en_US ?? "") !== "";
-        const label = item && hasFullItem ? `#${item.id}` : "";
         const classNames = [
           modifiers.active ? Classes.INTENT_PRIMARY : "",
-          modifiers.active || (item && itemIdActiveList?.includes(item.id)) ? Classes.ACTIVE : "",
-          item && hasFullItem ? qualityToColorClass(item.quality.type) : "",
+          modifiers.active || (item && idActiveList?.includes(item.id)) ? Classes.ACTIVE : "",
         ].filter(v => v.length > 0);
 
         return (
@@ -143,8 +68,8 @@ export function ItemInput(props: Props) {
             key={index}
             className={classNames.join(" ")}
             onClick={handleClick}
-            text={renderItemRendererText(item)}
-            label={label}
+            text={renderText(result)}
+            label={renderLabel(result)}
             disabled={disabled}
           />
         );
@@ -176,4 +101,44 @@ export function ItemInput(props: Props) {
       itemListRenderer={itemListRenderer}
     />
   );
+}
+
+function itemPredicate<T extends IQueryItemWithId>(_: string, result: IQueryItem<T>) {
+  return result.rank > -1;
+}
+
+function itemListRenderer<T extends IQueryItemWithId>(
+  params: IItemListRendererProps<IQueryItem<T>>,
+) {
+  const { items, itemsParentRef, renderItem } = params;
+  const renderedItems = items.map(renderItem).filter(renderedItem => renderedItem !== null);
+  if (renderedItems.length === 0) {
+    return (
+      <Menu ulRef={itemsParentRef}>
+        <li>
+          <H6>Queried Results</H6>
+        </li>
+        <li>
+          <em>No results found.</em>
+        </li>
+      </Menu>
+    );
+  }
+
+  return (
+    <Menu ulRef={itemsParentRef} className="item-input-menu">
+      <li>
+        <H6>Queried Results</H6>
+      </li>
+      {renderedItems}
+    </Menu>
+  );
+}
+
+export function QueryInput<T extends IQueryItemWithId>(props: Props<T>) {
+  const { autoFocus, onSelect, closeOnSelect, initialResults, inputValueRenderer } = props;
+
+  const [results, setResults] = useState<Array<IQueryItem<T>>>(initialResults ?? []);
+
+  return <QuerySuggest />;
 }
