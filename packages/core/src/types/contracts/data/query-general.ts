cbc3c921687a7eb860cb1a5ce22bae4cdbed73d3
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2020-10-21 18:54:41 -0700

    Start query-general contracts.

---
+++ packages/core/src/types/contracts/data/query-general.ts
@@ -0,0 +1,27 @@
+import { IShortItem } from "../../short-item";
+import { IShortPet } from "../../short-pet";
+import { IErrorResponse, IValidationErrorResponse } from "../index";
+import { IQueryRequest } from "./query";
+
+export type QueryGeneralRequest = IQueryRequest;
+
+export interface IQueryGeneralItemItem {
+  item: IShortItem | null;
+  pet: IShortPet | null;
+}
+
+export interface IQueryGeneralItem {
+  item: IQueryGeneralItemItem | null;
+  target: string;
+  rank: number;
+}
+
+export interface IQueryGeneralResponseData {
+  items: IQueryGeneralItem[];
+}
+
+export type QueryGeneralResponse =
+  | IQueryGeneralResponseData
+  | IErrorResponse
+  | IValidationErrorResponse
+  | null;
