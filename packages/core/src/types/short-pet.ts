8a1e166f46b6dd3d8e0eefe42df5a3f5f52569f3
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2020-10-23 08:46:16 -0700

    Add pet-quality.

--- packages/core/src/types/short-pet.ts
+++ packages/core/src/types/short-pet.ts
@@ -1,7 +1,14 @@
 export type PetId = number;
 
+export enum PetQuality {
+  Uncommon = 1,
+  Rare = 2,
+  Epic = 3,
+}
+
 export interface IShortPet {
   id: PetId;
   name: string;
   icon_url: string;
+  quality: PetQuality;
 }
