ccd4bc97cfe7bef5c2238159f145b7e0284153d3
Adrian Parker <ihsw.aparker@gmail.com>
2020-10-25 12:29:42 -0700

    Add missing.

--- packages/core/src/types/auction.ts
+++ packages/core/src/types/auction.ts
@@ -1,9 +1,12 @@
 import { ItemId } from "./item";
+import { PetId, PetQuality } from "./short-pet";
 
 export type AuctionId = number;
 
 export interface IAuction {
   itemId: ItemId;
+  pet_species_id: PetId;
+  pet_quality_id: PetQuality;
   buyout: number;
   buyoutPer: number;
   quantity: number;
