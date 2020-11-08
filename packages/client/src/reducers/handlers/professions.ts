33da5a0927bd2d4d2ce147dbca1370718dd0a166
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2020-11-08 12:18:58 -0700

    Add handler for receiving professions.

--- packages/client/src/reducers/handlers/professions.ts
+++ packages/client/src/reducers/handlers/professions.ts
@@ -1,8 +1,46 @@
-import { ProfessionsActions } from "../../actions/professions";
+import { ProfessionsActions, ReceiveGetProfessions } from "../../actions/professions";
+import { FetchLevel } from "../../types/main";
 import { IProfessionsState } from "../../types/professions";
 import { IKindHandlers, Runner } from "./index";
 
-export const handlers: IKindHandlers<IProfessionsState, ProfessionsActions> = {};
+export const handlers: IKindHandlers<IProfessionsState, ProfessionsActions> = {
+  professions: {
+    get: {
+      receive: (
+        state: IProfessionsState,
+        action: ReturnType<typeof ReceiveGetProfessions>,
+      ): IProfessionsState => {
+        if (action.payload === null || action.payload.response === null) {
+          return {
+            ...state,
+            professions: {
+              ...state.professions,
+              level: FetchLevel.failure,
+            },
+          };
+        }
+
+        return {
+          ...state,
+          professions: {
+            ...state.professions,
+            data: action.payload.response.professions,
+            level: FetchLevel.success,
+          },
+        };
+      },
+      request: (state): IProfessionsState => {
+        return {
+          ...state,
+          professions: {
+            ...state.professions,
+            level: FetchLevel.fetching,
+          },
+        };
+      },
+    },
+  },
+};
 
 export const run: Runner<IProfessionsState, ProfessionsActions> = (
   state: IProfessionsState,
