3bd6ce240cfc9d34f3db3ae56ecc3c24f1c88081
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2020-11-09 18:53:33 -0700

    Fix sorting.

--- packages/client/src/reducers/handlers/professions.ts
+++ packages/client/src/reducers/handlers/professions.ts
@@ -23,9 +23,13 @@ export const handlers: IKindHandlers<IProfessionsState, ProfessionsActions> = {
 
           return {
             ...state.professions,
-            data: action.payload.professions.response.professions.sort((a, b) =>
-              a.name.localeCompare(b.name),
-            ),
+            data: action.payload.professions.response.professions.sort((a, b) => {
+              if (a.id === b.id) {
+                return 0;
+              }
+
+              return a.id > b.id ? 1 : -1;
+            }),
             level: FetchLevel.success,
           };
         })();
