75bbb2581f6c4943e955c9549a473a6372cfa345
Adrian Parker <ihsw.aparker@gmail.com>
2020-11-17 07:01:14 -0700

    Continue recipe-popover in professions-tree sidebar.

--- packages/client/src/components/entry-point/Professions/ProfessionsTree/RecipePopover.tsx
+++ packages/client/src/components/entry-point/Professions/ProfessionsTree/RecipePopover.tsx
@@ -1,33 +1,22 @@
 import React from "react";
 
 import { Popover, PopoverInteractionKind, Position } from "@blueprintjs/core";
-import { IShortItem } from "@sotah-inc/core";
+import { IShortSkillTierCategoryRecipe } from "@sotah-inc/core";
 
-import { IItemClasses } from "../../../../types/global";
 import { ItemLink } from "../../../util/ItemLink";
 import { ItemPopoverContent } from "../../../util/ItemPopover/ItemPopoverContent";
 
-export interface IStateProps {
-  itemClasses: IItemClasses;
-}
-
-type ItemTextFormatterResult = string | React.ReactNode;
-
 export interface IOwnProps {
-  item: IShortItem;
+  recipe: IShortSkillTierCategoryRecipe;
 
-  onItemClick?: () => void;
-  itemTextFormatter?: (itemText: string) => ItemTextFormatterResult;
-  position?: Position;
-  interactive?: boolean;
+  onClick?: () => void;
 }
 
-type Props = Readonly<IStateProps & IOwnProps>;
+type Props = Readonly<IOwnProps>;
 
 export class RecipePopover extends React.Component<Props> {
   public static defaultProps: Partial<Props> = {
-    itemTextFormatter: (itemText: string) => itemText,
-    onItemClick: () => {
+    onClick: () => {
       return;
     },
   };
