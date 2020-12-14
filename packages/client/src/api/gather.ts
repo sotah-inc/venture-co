9118c7fcddf08fd20bd97796656ebac6476b84ec
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2020-12-13 20:34:09 -0700

    Dump version.

--- packages/client/src/api/gather.ts
+++ packages/client/src/api/gather.ts
@@ -4,47 +4,6 @@ import "isomorphic-fetch";
 import getConfig from "next/config";
 import queryString, { StringifiableRecord } from "query-string";
 
-interface IConfig {
-  publicRuntimeConfig?: {
-    publicApiEndpoint: string;
-  };
-  serverRuntimeConfig?: {
-    serverApiEndpoint: string;
-  };
-}
-
-const hostname: string = (() => {
-  if (typeof window === "undefined") {
-    return "";
-  }
-
-  return window.location.hostname;
-})();
-
-const defaultApiEndpoint = "https://api.sotah.info";
-export const getApiEndpoint: () => string = () => {
-  const { publicRuntimeConfig, serverRuntimeConfig }: IConfig = getConfig();
-  if (
-    typeof serverRuntimeConfig !== "undefined" &&
-    typeof serverRuntimeConfig.serverApiEndpoint !== "undefined"
-  ) {
-    return serverRuntimeConfig.serverApiEndpoint;
-  }
-
-  if (
-    typeof publicRuntimeConfig !== "undefined" &&
-    typeof publicRuntimeConfig.publicApiEndpoint !== "undefined"
-  ) {
-    return publicRuntimeConfig.publicApiEndpoint;
-  }
-
-  if (hostname === "localhost") {
-    return defaultApiEndpoint;
-  }
-
-  return defaultApiEndpoint;
-};
-
 export interface IGatherOptions<T> {
   headers?: Headers;
   body?: T;
