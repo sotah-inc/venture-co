746d57ef89cca75cd9cd3f54d641f54adc06ede1
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2020-12-24 15:43:37 -0700

    Add missing.

--- packages/core/src/types/contracts/data/recipe-price-histories.ts
+++ packages/core/src/types/contracts/data/recipe-price-histories.ts
@@ -1,8 +1,10 @@
+import { IPriceLimits } from "../../prices";
 import { IRecipePriceHistories } from "../../recipe-prices";
 import { IValidationErrorResponse } from "../index";
 
 export interface IGetRecipePriceHistoriesResponseData {
   history: IRecipePriceHistories;
+  overallPriceLimits: IPriceLimits;
 }
 
 export type GetRecipePriceHistoriesResponse =
