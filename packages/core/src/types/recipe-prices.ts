eef85134126ea95eddf1371fae3d0b22c74f27a0
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2020-12-24 18:44:06 -0700

    Fix typo.

--- packages/core/src/types/recipe-prices.ts
+++ packages/core/src/types/recipe-prices.ts
@@ -1,8 +1,20 @@
-export interface IRecipePrices {
+export interface IRecipeItemItemPrices {
   min_buyout_per: number;
   max_buyout_per: number;
   average_buyout_per: number;
   median_buyout_per: number;
+}
+
+export interface IRecipeItemPrices {
+  id: number;
+  prices: IRecipeItemItemPrices;
+}
+
+export interface IRecipePrices {
+  crafted_item_prices: IRecipeItemPrices;
+  alliance_crafted_item_prices: IRecipeItemPrices;
+  horde_crafted_item_prices: IRecipeItemPrices;
+  total_reagent_prices: IRecipeItemItemPrices;
 }
 
 export interface IRecipePriceHistory {
