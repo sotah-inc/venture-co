befc9b43767d5b9218539f0e978555055e4ceec7
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2020-12-26 11:33:30 -0700

    Refine get-recipe-price-histories response contract.

--- packages/core/src/types/contracts/data/recipe-price-histories.ts
+++ packages/core/src/types/contracts/data/recipe-price-histories.ts
@@ -1,3 +1,4 @@
+import { ItemId } from "../../item";
 import { IPriceLimits } from "../../prices";
 import { IRecipePriceHistories } from "../../recipe-prices";
 import { IValidationErrorResponse } from "../index";
@@ -5,6 +6,7 @@ import { IValidationErrorResponse } from "../index";
 export interface IGetRecipePriceHistoriesResponseData {
   history: IRecipePriceHistories;
   overallPriceLimits: IPriceLimits;
+  recipeItemIds: { [key: number]: ItemId[] };
 }
 
 export type GetRecipePriceHistoriesResponse =
