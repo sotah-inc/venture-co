1d34b53d02abe4bdedd3ae8134b5bbe8af16d0a3
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-01-01 10:06:28 -0700

    Update get-recipe-price-histories with item-price-histories data.

--- packages/core/src/types/contracts/data/recipe-price-histories.ts
+++ packages/core/src/types/contracts/data/recipe-price-histories.ts
@@ -1,12 +1,19 @@
 import { ItemId } from "../../item";
-import { IPriceLimits } from "../../prices";
+import { IItemPriceHistories } from "../../item-prices";
+import { IPriceLimits, IPricesFlagged } from "../../prices";
 import { IRecipePriceHistories } from "../../recipe-prices";
 import { IValidationErrorResponse } from "../index";
 
 export interface IGetRecipePriceHistoriesResponseData {
-  history: IRecipePriceHistories;
-  overallPriceLimits: IPriceLimits;
-  recipeItemIds: { [key: number]: ItemId[] };
+  recipeData: {
+    history: IRecipePriceHistories;
+    overallPriceLimits: IPriceLimits;
+    recipeItemIds: { [key: number]: ItemId[] };
+  };
+  itemData: {
+    history: IItemPriceHistories<IPricesFlagged>;
+    aggregatePriceLimits: IPriceLimits;
+  };
 }
 
 export type GetRecipePriceHistoriesResponse =
