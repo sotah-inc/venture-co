56aac7d1e3703fbcf93f29fe5255c3f0e59a04ea
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-01-21 07:04:25 -0700

    Add new useful func.

--- packages/client/src/api/token-history.ts
+++ packages/client/src/api/token-history.ts
@@ -1,4 +1,10 @@
-import { GetRegionTokenHistoryResponse, IRegionTokenHistory, RegionName } from "@sotah-inc/core";
+import {
+  GetRegionTokenHistoryResponse,
+  GetShortTokenHistoryResponse,
+  IRegionTokenHistory,
+  IShortTokenHistory,
+  RegionName,
+} from "@sotah-inc/core";
 import * as HTTPStatus from "http-status";
 
 import { getApiEndpoint } from "./config";
@@ -16,6 +22,28 @@ export const getRegionTokenHistory = async (
     headers: new Headers({ "content-type": "application/json" }),
     method: "GET",
     url: `${getApiEndpoint()}/token-history/${regionName}`,
+  });
+
+  switch (status) {
+    case HTTPStatus.OK:
+      break;
+    default:
+      return { history: null, error: "Failure" };
+  }
+
+  return { history: body!.history, error: null };
+};
+
+export interface IGetShortTokenHistoryResult {
+  history: IShortTokenHistory | null;
+  error: string | null;
+}
+
+export const getShortTokenHistory = async (): Promise<IGetShortTokenHistoryResult> => {
+  const { body, status } = await gather<null, GetShortTokenHistoryResponse>({
+    headers: new Headers({ "content-type": "application/json" }),
+    method: "GET",
+    url: `${getApiEndpoint()}/token-history`,
   });
 
   switch (status) {
