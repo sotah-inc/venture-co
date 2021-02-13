ef33789bd46f2c40f9342480857aedb9f20eee15
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-02-13 13:21:40 -0700

    Remove unnecessary.

--- packages/core/src/types/contracts/data/recipe-price-histories.ts
+++ packages/core/src/types/contracts/data/recipe-price-histories.ts
@@ -1,18 +1,16 @@
 import { ItemId } from "../../item";
 import { IItemPriceHistories } from "../../item-prices";
-import { IPriceLimits, IPricesFlagged } from "../../prices";
+import { IPricesFlagged } from "../../prices";
 import { IRecipePriceHistories } from "../../recipe-prices";
 import { IValidationErrorResponse } from "../index";
 
 export interface IGetRecipePriceHistoriesResponseData {
   recipeData: {
     history: IRecipePriceHistories;
-    overallPriceLimits: IPriceLimits;
     recipeItemIds: { [key: number]: ItemId[] };
   };
   itemData: {
     history: IItemPriceHistories<IPricesFlagged>;
-    aggregatePriceLimits: IPriceLimits;
   };
 }
 
