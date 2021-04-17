ff89645ad8989c9f0aa42a343ee8fe6cce9cb3c9
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-04-17 15:38:18 -0700

    Add missing.

--- bin/start-sotah-server-api.sh
+++ bin/start-sotah-server-api.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
-export $(cat ~/bin/battlenet-creds.env | xargs) \
+docker-compose stop sotah-server-api \
+  && docker-compose rm -fv sotah-server-api \
+  && export $(cat ~/bin/battlenet-creds.env | xargs) \
   && docker-compose up -d sotah-server-api \
   && docker-compose logs -f sotah-server-api
