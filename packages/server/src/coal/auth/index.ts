2f7145ecc2522cdeb99245d462388fe5e76a9342
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-05-16 11:36:50 -0700

    Fix typo.

--- packages/server/src/coal/auth/index.ts
+++ packages/server/src/coal/auth/index.ts
@@ -63,6 +63,8 @@ export function auth(dbConn: Connection) {
       .getRepository(User)
       .findOne({ where: { firebaseUid: verifyIdTokenResult.firebaseUid } });
     if (user === undefined) {
+      next();
+
       return;
     }
 
