9738ff0a7612c56a6d674e1ee473747fd11cba38
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-05-18 19:51:23 -0700

    Update to support public and private endpoints.

--- bin/start-sotah-api.sh
+++ bin/start-sotah-api.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 
 export $(cat ~/bin/battlenet-creds.env | xargs) \
-  && docker-compose run --service-ports sotah-api
+  && docker-compose up -d sotah-api-public \
+  && docker-compose up -d sotah-api-private \
+  && docker-compose logs -f sotah-api-public sotah-api-private
