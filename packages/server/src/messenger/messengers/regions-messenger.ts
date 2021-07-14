9dcabe3bce7e3a5d9424d8d998c371e040af6f6b
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-07-14 13:18:03 -0700

    Fix naming, ordering.

--- packages/server/src/messenger/messengers/regions-messenger.ts
+++ packages/server/src/messenger/messengers/regions-messenger.ts
@@ -1,7 +1,10 @@
 import {
   IConnectedRealmComposite,
   IConnectedRealmModificationDates,
+  IGameVersionTuple,
   IRegionComposite,
+  IRegionVersionConnectedRealmTuple,
+  IRegionVersionRealmTuple,
   IRegionVersionTuple,
 } from "@sotah-inc/core";
 
@@ -15,71 +18,69 @@ import { Message, ParseKind } from "../message";
 import { BaseMessenger } from "./base";
 
 enum subjects {
-  status = "status",
-  connectedRealms = "connectedRealms",
-  resolveConnectedRealm = "resolveConnectedRealm",
-  queryRealmModificationDates = "queryRealmModificationDates",
   connectedRealmModificationDates = "connectedRealmModificationDates",
+  connectedRealms = "connectedRealms",
+  queryRealmModificationDates = "queryRealmModificationDates",
+  resolveConnectedRealm = "resolveConnectedRealm",
+  status = "status",
   validateGameVersion = "validateGameVersion",
-  validateRegionRealm = "validateRegionRealm",
   validateRegionConnectedRealm = "validateRegionConnectedRealm",
+  validateRegionRealm = "validateRegionRealm",
 }
 
 export class RegionsMessenger extends BaseMessenger {
-  public getStatus(tuple: IRegionVersionTuple): Promise<Message<IRegionComposite>> {
-    return this.request(subjects.status, {
-      body: JSON.stringify(tuple),
-    });
+  public connectedRealmModificationDates(): Promise<Message<IRealmModificationDatesResponse>> {
+    return this.request(subjects.connectedRealmModificationDates);
   }
 
-  public getConnectedRealms(
-    tuple: IRegionVersionTuple,
-  ): Promise<Message<IConnectedRealmComposite[]>> {
+  public connectedRealms(tuple: IRegionVersionTuple): Promise<Message<IConnectedRealmComposite[]>> {
     return this.request(subjects.connectedRealms, {
       body: JSON.stringify(tuple),
       parseKind: ParseKind.GzipJsonEncoded,
     });
   }
 
-  public validateRegionConnectedRealm(
-    tuple: IRegionConnectedRealmTuple,
-  ): Promise<Message<IValidateRegionConnectedRealmResponse>> {
-    return this.request(subjects.validateRegionConnectedRealm, {
-      body: JSON.stringify(tuple),
-    });
+  public queryRealmModificationDates(
+    tuple: IRegionVersionConnectedRealmTuple,
+  ): Promise<Message<IConnectedRealmModificationDates>> {
+    return this.request(subjects.queryRealmModificationDates, { body: JSON.stringify(tuple) });
   }
 
   public resolveConnectedRealm(
-    tuple: IRegionRealmTuple,
+    tuple: IRegionVersionRealmTuple,
   ): Promise<Message<IResolveConnectedRealmResponse>> {
     return this.request(subjects.resolveConnectedRealm, {
       body: JSON.stringify(tuple),
     });
   }
 
-  public validateRegionRealm(
-    tuple: IRegionRealmTuple,
-  ): Promise<Message<ValidateRegionRealmResponse>> {
-    return this.request(subjects.validateRegionRealm, {
+  public status(tuple: IRegionVersionTuple): Promise<Message<IRegionComposite>> {
+    return this.request(subjects.status, {
       body: JSON.stringify(tuple),
     });
   }
 
   public validateGameVersion(
-    tuple: IRegionRealmTuple,
+    tuple: IGameVersionTuple,
   ): Promise<Message<ValidateRegionRealmResponse>> {
     return this.request(subjects.validateGameVersion, {
       body: JSON.stringify(tuple),
     });
   }
 
-  public queryRealmModificationDates(
-    tuple: IRegionConnectedRealmTuple,
-  ): Promise<Message<IConnectedRealmModificationDates>> {
-    return this.request(subjects.queryRealmModificationDates, { body: JSON.stringify(tuple) });
+  public validateRegionConnectedRealm(
+    tuple: IRegionVersionConnectedRealmTuple,
+  ): Promise<Message<IValidateRegionConnectedRealmResponse>> {
+    return this.request(subjects.validateRegionConnectedRealm, {
+      body: JSON.stringify(tuple),
+    });
   }
 
-  public getConnectedRealmModificationDates(): Promise<Message<IRealmModificationDatesResponse>> {
-    return this.request(subjects.connectedRealmModificationDates);
+  public validateRegionRealm(
+    tuple: IRegionVersionRealmTuple,
+  ): Promise<Message<ValidateRegionRealmResponse>> {
+    return this.request(subjects.validateRegionRealm, {
+      body: JSON.stringify(tuple),
+    });
   }
 }
