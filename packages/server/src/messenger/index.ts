8536102153fa423e7ae92b2bf2305ff5b269bf73
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-07-24 16:57:36 -0700

    Fill in remaining messengers.

--- packages/server/src/messenger/index.ts
+++ packages/server/src/messenger/index.ts
@@ -8,18 +8,25 @@ import {
   PricelistHistoryMessenger,
   ProfessionsMessenger,
   RegionsMessenger,
+  BootMessenger,
+  StatsMessenger,
+  TokensMessenger,
 } from "./messengers";
 
 export * from "./messengers";
 export * from "./contracts";
 
 export interface IMessengers {
-  auctions: LiveAuctionsMessenger;
+  boot: BootMessenger;
   general: GeneralMessenger;
   items: ItemsMessenger;
+  liveAuctions: LiveAuctionsMessenger;
   pets: PetsMessenger;
+  pricelistHistory: PricelistHistoryMessenger;
   professions: ProfessionsMessenger;
   regions: RegionsMessenger;
+  stats: StatsMessenger;
+  tokens: TokensMessenger;
 }
 
 export function getMessengers(conn: NatsConnection): IMessengers {
@@ -28,11 +35,15 @@ export function getMessengers(conn: NatsConnection): IMessengers {
   const pricelistHistoryMessenger = new PricelistHistoryMessenger(conn);
 
   return {
-    auctions: new LiveAuctionsMessenger(conn, itemsMessenger, petsMessenger),
-    items: itemsMessenger,
-    pets: petsMessenger,
+    boot: new BootMessenger(conn),
     general: new GeneralMessenger(conn, itemsMessenger, petsMessenger, pricelistHistoryMessenger),
+    items: itemsMessenger,
+    liveAuctions: new LiveAuctionsMessenger(conn, itemsMessenger, petsMessenger),
+    pets: petsMessenger,
+    pricelistHistory: new PricelistHistoryMessenger(conn),
     professions: new ProfessionsMessenger(conn, itemsMessenger),
     regions: new RegionsMessenger(conn),
+    stats: new StatsMessenger(conn),
+    tokens: new TokensMessenger(conn),
   };
 }
