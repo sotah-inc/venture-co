c425741ee883e89c2b63cd22dd3bab36798b1c38
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-09-28 06:49:21 -0700

    Add missing.

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -22,3 +22,12 @@ export function normalizeLimits(input: ILimits): ILimits {
     upper: Math.pow(10, Math.ceil(Math.log10(input.upper))),
   };
 }
+
+export enum LogLevel {
+  TRACE = 0,
+  DEBUG = 1,
+  INFO = 2,
+  WARN = 3,
+  ERROR = 4,
+  SILENT = 5,
+}
