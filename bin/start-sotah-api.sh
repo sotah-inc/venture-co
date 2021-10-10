a636fb5a58ff788721972d166c8897994ce87810
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-10-10 15:18:25 -0700

    Misc.

--- bin/start-sotah-api.sh
+++ bin/start-sotah-api.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
 export $(cat ~/bin/battlenet-creds.env | xargs) \
-  && docker-compose up -d sotah-api-public \
-  && docker-compose up -d sotah-api-private \
-  && docker-compose logs -f sotah-api-public sotah-api-private
+  && docker compose up -d sotah-api-public \
+  && docker compose up -d sotah-api-private \
+  && docker compose logs -f sotah-api-public sotah-api-private
