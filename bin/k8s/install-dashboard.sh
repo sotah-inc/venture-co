098d58b222467ac43a97454d23e8774c15bd5bc1
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-10-31 15:01:29 -0700

    Add helpful.

---
+++ bin/k8s/install-dashboard.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+kubectl apply \
+  -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.3.1/aio/deploy/recommended.yaml
---
+++ bin/k8s/uninstall-k3s.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+/usr/local/bin/k3s-uninstall.sh
