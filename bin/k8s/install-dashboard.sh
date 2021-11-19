961cc95664ce8c9a2d2349e8740fe2fc287b4c7c
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-11-19 07:07:08 -0700

    Add patching of token-ttl for development.

--- bin/k8s/install-dashboard.sh
+++ bin/k8s/install-dashboard.sh
@@ -2,3 +2,8 @@
 
 kubectl apply \
   -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.3.1/aio/deploy/recommended.yaml
+
+kubectl patch deployment kubernetes-dashboard \
+  -n kubernetes-dashboard \
+  --type json \
+  -p '[{ "op": "add", "path": "/spec/template/spec/containers/0/args/2", "value": "--token-ttl=0" }]'
