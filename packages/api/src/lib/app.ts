06cec1b85ccc654bec19388860fc5b22c3414623
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-11-19 16:56:22 -0700

    Remove logger from initial log line.

--- packages/api/src/lib/app.ts
+++ packages/api/src/lib/app.ts
@@ -22,7 +22,7 @@ export interface IOptions {
 export async function getApp(opts: IOptions): Promise<express.Express | null> {
   const { logger, natsHost, natsPort, dbHost, dbPassword } = opts;
 
-  logger.info("starting app", { opts });
+  logger.info("starting app", { ...opts, logger: undefined });
 
   // const errors = isGceEnv ? new ErrorReporting() : null;
 
